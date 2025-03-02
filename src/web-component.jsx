87805b1142242a80f25e940a0cfb3440a5f3691a
anshsx <erasearch.co@gmail.com>
2025-03-02 19:13:32 +0530

    Update web-component.jsx

--- src/web-component.jsx
+++ src/web-component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Widget from "@/src/components/Widget";
+import Widget from "./components/Widget";
 
 class WidgetWebComponent extends HTMLElement {
   connectedCallback() {
