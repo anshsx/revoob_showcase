e103829c485f3389267a04de774d79e85a2f2f3f
anshsx <erasearch.co@gmail.com>
2025-03-04 14:28:35 +0530

    Update web-component.jsx

--- src/web-component.jsx
+++ src/web-component.jsx
@@ -1,17 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Widget from "./components/Widget";
+import ReactDom from "react-dom/client";
+import { Widget } from "./components/Widget";
+
+export const normalizeAttribute = (attribute) => {
+  return attribute.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+};
 
 class WidgetWebComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
   connectedCallback() {
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    const mountPoint = document.createElement("div");
-    shadowRoot.appendChild(mountPoint);
+    const props = this.getPropsFromAttributes();
+    const root = ReactDom.createRoot(this.shadowRoot);
+    root.render(<Widget {...props} />);
+  }
 
-    const projectId = this.getAttribute("project-id"); // Get Project ID from attribute
-
-    const root = ReactDOM.createRoot(mountPoint);
-    root.render(<Widget projectId={projectId} />);
+  getPropsFromAttributes() {
+    const props = {};
+    for (const { name, value } of this.attributes) {
+      props[normalizeAttribute(name)] = value;
+    }
+    return props;
   }
 }
 
